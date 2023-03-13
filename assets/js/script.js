fd65a5ca8e055c3a08524015fcc3a86aee62bca5
Parker <parkerlamarbrook@gmail.com>
2023-03-12 21:11:56 -0600

    code is in and the program correctly produces random passwords. I just need to add comments to the remaining half of the code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,226 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// define arrays for all (upper and lower case) letters, numbers, and special characters that can be used in a generated password
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var characters = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", ":", "'", "<", ">", ",", ".", "?"];
+var lettersUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lettersLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+// prompt the user for desired password parameters: length, type (UC/LC letters, numbers, special characters)
+/* function promptParameters(){
+  // this prompt will ask the user how long they want their password to be (req. 8-128 chars.)
+  // the below lines will display a prompt asking for a password length, 8-128 chars. The text field will also display "password length". Parse int will convert the user-entered string to a number with base 10.
+  var passwordLength = parseInt(
+    prompt(" Please specify a desired password length from 8 to 128 characters.", "password length"),
+    10
+  );
+
+  // now we need to validate the user's input and confirm their entry is usable data.
+  // first, check to confirm the user's input consists only of numeric digits, 0 - 9, and no letters or special characters. If it does not, show an alert, then go back to the prompt.
+  if (isNaN(passwordLength) === true){
+    alert("❗ Password length must be specified using only numeric characters (0 - 9). ❗");
+    promptParameters();
+  };
+
+  // second, check to confirm the user's input is 8 characters or greater in length. If it is not, show an alert, then go back to the prompt.
+  // note: I'm using Logical NOT "!" and ">=" instead of simply "<" to directly align with the logic above, even though it is slightly less efficient.
+  if (!(passwordLength >= 8)){
+    alert("❗ Password length must be no fewer than 8 characters. ❗");
+    promptParameters();
+  };
+
+  // third, check to confirm the user's input is 128 characters in length or shorter. If it is not, show an alert, then go back to the prompt.
+  if (!(passwordLength <= 128)){
+    alert("❗ Password length must be no greater than 128 characters. ❗");
+    promptParameters();
+  }; */
+
+  // call the function that asks the user to specify character type
+  // this function is kept separate so that the user doesn't have to start over at "promptParameters()" if they fail to select any character type. It will instead bring up the first selection for numbers after the alert. 
+  
+
+  // i created a second function specifically to prompt the user for their desired character types so that if the user selects none of the four types, they do not have to enter a password length again.
+  
+
+function generatePassword(){
+  // this prompt will ask the user how long they want their password to be (req. 8-128 chars.)
+  // the below lines will display a prompt asking for a password length, 8-128 chars. The text field will also display "password length". Parse int will convert the user-entered string to a number with base 10.
+  var passwordLength = parseInt(
+    prompt(" Please specify a desired password length from 8 to 128 characters.", "password length"),
+    10
+  );
+
+  // now we need to validate the user's input and confirm their entry is usable data.
+  // first, check to confirm the user's input consists only of numeric digits, 0 - 9, and no letters or special characters. If it does not, show an alert, then go back to the prompt.
+  if (isNaN(passwordLength) === true){
+    alert("❗ Password length must be specified using only numeric characters (0 - 9). ❗");
+    generatePassword();
+  };
+
+  // second, check to confirm the user's input is 8 characters or greater in length. If it is not, show an alert, then go back to the prompt.
+  // note: I'm using Logical NOT "!" and ">=" instead of simply "<" to directly align with the logic above, even though it is slightly less efficient.
+  if (!(passwordLength >= 8)){
+    alert("❗ Password length must be no fewer than 8 characters. ❗");
+    generatePassword();
+  };
+
+  // third, check to confirm the user's input is 128 characters in length or shorter. If it is not, show an alert, then go back to the prompt.
+  if (!(passwordLength <= 128)){
+    alert("❗ Password length must be no greater than 128 characters. ❗");
+    generatePassword();
+  };
+  // define what types of characters the password will be comprised of
+  // ask user if password should include numbers using window.confirm() method, then store the value (true/false boolean) in a var for later.
+  var useNumbers = window.confirm("Click 'OK' if you'd like your password to include numbers.\n\nClick 'Cancel' to exclude them.");
+
+  // ask user if password should include special characters using window confirm, then store the value (true/false boolean) in a var for later.
+  var useCharacters = window.confirm("Click 'OK' if you'd like your password to include special characters.\n\nClick 'Cancel' to exclude them.");
+
+  // ask user if password should include *uppercase* letters using window confirm, then store the value (true/false boolean) in a var for later.
+  var useUppercase = window.confirm("Click 'OK' if you'd like your password to include uppercase letters.\n\nClick 'Cancel' to exclude them.");
+
+  // ask user if password should include *lowercase* letters using window confirm, then store the value (true/false boolean) in a var for later.
+  var useLowercase = window.confirm("Click 'OK' if you'd like your password to include lowercase letters.\n\nClick 'Cancel' to exclude them.");
+
+  // check to confirm that the user selected *at least* one of the above four types of characters for their password to contain
+  // if they did not, show an alert stating the requirement
+  if (!useNumbers && !useCharacters && !useUppercase && !useLowercase){
+    alert("❗ Please select one or more character-types in order to proceed with password generation. ❗")
+    generatePassword();
+  }
+  
+  // now that all the parameters pertaining to password generation have been gathered from the user, we can store the values as properties of the object passwordParameters to be used later.
+  var passwordParameters = {
+    useNumbers: useNumbers,
+    useCharacters: useCharacters,
+    useUppercase: useUppercase,
+    useLowercase: useLowercase,
+  };
+
+  // this variable is set as an empty string to begin with and will be used as bucket holding all characters associated with the character types the user specified
+  var generatedPassword = '';
+
+  if (passwordParameters.useCharacters){
+    for (var i=0; i<characters.length; i++){
+      generatedPassword += characters[i];
+    }
+  }
+
+  if (passwordParameters.useNumbers){
+    for (var i=0; i<numbers.length; i++){
+      generatedPassword += numbers[i];
+    }
+  }
+
+  if (passwordParameters.useUppercase){
+    for (var i=0; i<lettersUppercase.length; i++){
+      generatedPassword += lettersUppercase[i];
+    }
+  }
+
+  if (passwordParameters.useLowercase){
+    for (var i=0; i<lettersLowercase.length; i++){
+      generatedPassword += lettersLowercase[i];
+    }
+  }
+
+  var finalPassword = '';
+
+
+  for (var i=0; i<passwordLength; i++){
+    var randomIndex = Math.floor(Math.random()*generatedPassword.length);
+    finalPassword += generatedPassword[randomIndex];
+  }
+
+  return finalPassword;
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ----------------------------- graveyard. code saved if needed later ----------------------------- \\
+
+// define what types of characters the password will be comprised of
+  // ask user if password should include numbers using window.confirm() method, then store the value (true/false boolean) in a var for later.
+  // var useNumbers = window.confirm("Click 'OK' if you'd like your password to include numbers.\n\nClick 'Cancel' to exclude them.");
+
+  // ask user if password should include special characters using window confirm, then store the value (true/false boolean) in a var for later.
+  // var useCharacters = window.confirm("Click 'OK' if you'd like your password to include special characters.\n\nClick 'Cancel' to exclude them.");
+
+  // ask user if password should include *uppercase* letters using window confirm, then store the value (true/false boolean) in a var for later.
+  // var useUppercase = window.confirm("Click 'OK' if you'd like your password to include uppercase letters.\n\nClick 'Cancel' to exclude them.");
+
+  // ask user if password should include *lowercase* letters using window confirm, then store the value (true/false boolean) in a var for later.
+  // var useLowercase = window.confirm("Click 'OK' if you'd like your password to include lowercase letters.\n\nClick 'Cancel' to exclude them.");
+
+  // check to confirm that the user selected *at least* one of the above four types of characters for their password to contain
+  // if they did not, show an alert stating the requirement
+  // if (!useNumbers && !useCharacters && !useUppercase && !useLowercase){
+  //   alert("❗ Please select one or more character-types in order to proceed with password generation ❗")
+  //    promptParameters();
+  // }
+
+
+
+/* // i created a second function specifically to prompt the user for their desired character types so that if the user selects none of the four types, they do not have to enter a password length again.
+  function promptCharacters(){
+    // define what types of characters the password will be comprised of
+    // ask user if password should include numbers using window.confirm() method, then store the value (true/false boolean) in a var for later.
+    var useNumbers = window.confirm("Click 'OK' if you'd like your password to include numbers.\n\nClick 'Cancel' to exclude them.");
+  
+    // ask user if password should include special characters using window confirm, then store the value (true/false boolean) in a var for later.
+    var useCharacters = window.confirm("Click 'OK' if you'd like your password to include special characters.\n\nClick 'Cancel' to exclude them.");
+  
+    // ask user if password should include *uppercase* letters using window confirm, then store the value (true/false boolean) in a var for later.
+    var useUppercase = window.confirm("Click 'OK' if you'd like your password to include uppercase letters.\n\nClick 'Cancel' to exclude them.");
+  
+    // ask user if password should include *lowercase* letters using window confirm, then store the value (true/false boolean) in a var for later.
+    var useLowercase = window.confirm("Click 'OK' if you'd like your password to include lowercase letters.\n\nClick 'Cancel' to exclude them.");
+  
+    // check to confirm that the user selected *at least* one of the above four types of characters for their password to contain
+    // if they did not, show an alert stating the requirement
+    if (!useNumbers && !useCharacters && !useUppercase && !useLowercase){
+      alert("❗ Please select one or more character-types in order to proceed with password generation ❗")
+      promptCharacters();
+    }
+
+    var passwordParameters = {
+      useNumbers: useNumbers,
+      useCharacters: useCharacters,
+      useUppercase: useUppercase,
+      useLowercase: useLowercase,
+    };
+
+    return passwordParameters;
+    console.log(passwordParameters);    
+  } */
+
+
+  
+  // var passwordPrompt = prompt("❗ Please specify your desired password length from 8 to 128 characters. ❗", "password length");
+  // var passwordLength = parseInt(passwordPrompt, 10);
